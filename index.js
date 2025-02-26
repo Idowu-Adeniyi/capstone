7744bfe6aaeb08e48de9e4d27652f0836a3db4d4
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2025-02-25 23:54:59 -0500

    setting up the dir path

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const path = require("path");
 const app = express(); // create express application
 const port = process.env.PORT || "8888"; // set up a port number to run the application from
 
+//set up express app to use pug as template engine
+app.set("views", path.join(__dirname, "templates"));
+
 //Page routes
 app.get("/", (request, response) => {
   response.status(200).send("Hello");
